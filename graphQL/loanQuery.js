eddb627c47550e71f9588a02f974193114e162c5
Annie Ausen <ausen.annie@gmail.com>
2021-10-13 16:18:23 -0500

    api query start

--- graphQL/loanQuery.js
+++ graphQL/loanQuery.js
@@ -1,46 +1,39 @@
-// import gql from "graphql-tag";
-fetch('https://api.kivaws.org/graphql', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ query: "{lend {loan (id: 1568001){id name}}}" }),
-})
-  .then(res => res.json())
-  .then(res => console.log(res.data));
+import gql from "graphql-tag";
 
-// // Function to query the Kiva Graphql API and return results
+// Function to query the Kiva Graphql API and return results
 
-// const LOAN_QUERY = gql`
-//     query LOAN_QUERY{
-//         lend{
-//             loans (filters: {gender: female, country: ["KE"]}, limit:100){
-//                 totalCount
-//                 values {
-//                     name
-//                     loanAmount
-//                     image {
-//                         url(presetSize:small)
-//                     }
-//                     activity{
-//                         name
-//                     }
-//                     geocode{
-//                         country{
-//                             isoCode
-//                             name
-//                         }
-//                     }
-//                     lenders (limit: 10) {
-//                         totalCount
-//                     }
-//                     ... on LoanPartner {
-//                         partnerName
-//                     }
-//                     ... on LoanDirect {
-//                         trusteeName
-//                     }
-//                 }
-//             }
-//         }
-//     }`
+const LOAN_QUERY = gql`
+    query LOAN_QUERY{
+        lend{
+            loans (filters: {gender: female, country: ["KE"]}, limit:100){
+                totalCount
+                values {
+                    name
+                    loanAmount
+                    image {
+                        url(presetSize:small)
+                    }
+                    activity{
+                        name
+                    }
+                    geocode{
+                        country{
+                            isoCode
+                            name
+                        }
+                    }
+                    lenders (limit: 10) {
+                        totalCount
+                    }
+                    ... on LoanPartner {
+                        partnerName
+                    }
+                    ... on LoanDirect {
+                        trusteeName
+                    }
+                }
+            }
+        }
+    }`
 
-// export default LOAN_QUERY;
+export default LOAN_QUERY;
